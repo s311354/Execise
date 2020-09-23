b0d74a8badfd59559ff1e3530fe6c8b255a49496
s311354 <qazqazqaz850@gmail.com>
2020-09-23 08:51:03 +0800

    Exercise. Text manipulation

---
+++ Text_Manipulation/name.c
@@ -0,0 +1,7 @@
+/* File generated automatically - do not edit*/
+const char *NAME_name[] = {
+ "state_a",
+ "state_b",
+ "state_c",
+ "state_d"
+};
---
+++ Text_Manipulation/name.h
@@ -0,0 +1,8 @@
+/* file generated automatically - do not edit*/ 
+extern const char *NAME_name[]typedef emu {
+state_a, 
+state_b, 
+state_c, 
+state_d
+} NAME; 
+
